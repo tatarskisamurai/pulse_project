7f0a206f1d43b092d3b889de4ad4c6f64f95eca2
tatarskisamurai <dvejder806@gmail.com>
2024-03-23 12:16:20 +0300

    add popup

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,19 @@ $(document).ready(function(){
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
    });
+
+   $('[data-modal=consultation]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('slow');
+   })
+
+   $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+   })
+
+   $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+         $('.overlay, #order').fadeIn('slow');
+      })
+   })
  });
