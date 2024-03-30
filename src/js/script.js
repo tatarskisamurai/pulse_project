42eeb1eb70cc0944f5e9d1be12c01ab36f9e6297
tatarskisamurai <dvejder806@gmail.com>
2024-03-30 16:08:57 +0300

    add validtion form

--- src/js/script.js
+++ src/js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
          })
       })
-   }
+   };
    toggleSlide('.catalog-item__link');
    toggleSlide('.catalog-item__back');
    
@@ -37,16 +37,49 @@ $(document).ready(function(){
 
    $('[data-modal=consultation]').on('click', function(){
       $('.overlay, #consultation').fadeIn('slow');
-   })
+   });
 
    $('.modal__close').on('click', function(){
       $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
-   })
+   });
+
+   
 
    $('.button_mini').each(function(i){
       $(this).on('click', function(){
          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
          $('.overlay, #order').fadeIn('slow');
       })
-   })
+   });
+
+   function validateForms(form){
+      $(form).validate({
+          rules: {
+              name: {
+                  required: true,
+                  minlength: 2
+              },
+              phone: "required",
+              email: {
+                  required: true,
+                  email: true
+              }
+          },
+          messages: {
+              name: {
+                  required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+              phone: "Пожалуйста, введите свой номер телефона",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+              }
+          }
+      });
+   };
+
+   validateForms('#consultation-form');
+   validateForms('#consultation form');
+   validateForms('#order form');
  });
