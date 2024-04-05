385359cbe7050953e500990a63f5800e86a279dc
tatarskisamurai <dvejder806@gmail.com>
2024-04-06 00:17:39 +0300

    add scroll

--- src/js/script.js
+++ src/js/script.js
@@ -100,4 +100,18 @@ $(document).ready(function(){
       });
       return false;
   });
+
+  $(window).scroll(function() {
+   if ($(this).scrollTop() > 1600) {
+       $('.pageup').fadeIn();
+   } else {
+       $('.pageup').fadeOut();
+   }
+});
+
+   $("a[href=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+   });
  });
